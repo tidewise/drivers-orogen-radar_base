994386f51bb9aaf93dcc15e8987347731ce5c025
Afonso da Fonseca Braga <afonso.braga@tidewise.io>
2023-05-19 14:54:36 -0300

    chore: add support for negative stepsize, cleanup

--- test/echoes_to_frame_converter_task_test.rb
+++ test/echoes_to_frame_converter_task_test.rb
@@ -7,11 +7,46 @@ import_types_from "base"
 describe OroGen.radar_base.EchoesToFrameConverterTask do
     run_live
 
-    it "starts and outputs radar data" do
-        task = create_configure_and_start_task
+    it "starts and outputs single radar data" do
+        task = create_configure_task
+        syskit_start(task)
+
         output = expect_execution do
             syskit_write task.echo_port, @echo
         end.to do
+            have_one_new_sample(task.frame_port)
+        end
+        expected = File.binread(File.join(__dir__, "image1.bin"))
+
+        assert_equal expected, output.image.to_a.to_s,
+                     "single radar data output differs from expected image"
+    end
+
+    it "starts and outputs multiple radar data for a single frame" do
+        task = create_configure_task
+        task.poll do
+            syskit_write task.echo_port, @echo_part1
+            syskit_write task.echo_port, @echo_part2
+        end
+        syskit_start(task)
+
+        output = expect_execution.to do
+            have_one_new_sample(task.frame_port)
+        end
+        expected = File.binread(File.join(__dir__, "image2.bin"))
+
+        assert_equal expected, output.image.to_a.to_s,
+                     "multiple radar data output differs from expected image"
+    end
+
+    it "starts and outputs radar data with a negative stepsize" do
+        task = create_configure_task
+        task.poll do
+            syskit_write task.echo_port, @echo_inverted
+        end
+        syskit_start(task)
+
+        output = expect_execution.to do
             have_one_new_sample(task.frame_port)
         end
         expected = File.binread(File.join(__dir__, "image1.bin"))
@@ -20,31 +55,18 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
                      "single radar data output differs from expected image"
     end
 
-    it "starts and outputs multiple radar data for a single frame" do
-        task = create_configure_and_start_task
-        output = expect_execution do
-            syskit_write task.echo_port, @echo_part1
-            syskit_write task.echo_port, @echo_part2
-        end.to do
-            have_one_new_sample(task.frame_port)
-        end
-        expected = File.binread(File.join(__dir__, "image2.bin"))
-
-        assert_equal expected, "#{output.image.to_a}",
-                     "multiple radar data output differs from expected image"
-    end
-
     it "it rotates sample 90 degrees 5 times" do
-        task = create_task
-        syskit_configure_and_start(task)
+        task = create_configure_task
+        syskit_start(task)
 
         arrow = []
         64.times { arrow.concat [0] }
         arrow[0..7] = [0, 255, 0, 255, 0, 255, 0, 255]
         @echo_rotation[:sweep_data] = arrow
         @sensor2ref_pose.orientation = Eigen::Quaternion.new(1, 0, 0, 0)
+
+        syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
         output1 = expect_execution do
-            syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
             syskit_write task.echo_port, @echo_rotation
         end.to do
             have_one_new_sample(task.frame_port)
@@ -54,8 +76,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
         arrow[48..55] = [0, 255, 0, 255, 0, 255, 0, 255]
         @echo_rotation[:sweep_data] = arrow
         @sensor2ref_pose.orientation = Eigen::Quaternion.new(0.7071068, 0, 0, 0.7071068)
+
+        syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
         output2 = expect_execution do
-            syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
             syskit_write task.echo_port, @echo_rotation
         end.to do
             have_one_new_sample(task.frame_port)
@@ -65,8 +88,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
         arrow[32..39] = [0, 255, 0, 255, 0, 255, 0, 255]
         @echo_rotation[:sweep_data] = arrow
         @sensor2ref_pose.orientation = Eigen::Quaternion.new(0, 0, 0, 1)
+
+        syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
         output3 = expect_execution do
-            syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
             syskit_write task.echo_port, @echo_rotation
         end.to do
             have_one_new_sample(task.frame_port)
@@ -76,8 +100,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
         arrow[16..23] = [0, 255, 0, 255, 0, 255, 0, 255]
         @echo_rotation[:sweep_data] = arrow
         @sensor2ref_pose.orientation = Eigen::Quaternion.new(-0.7071068, 0, 0, 0.7071068)
+
+        syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
         output4 = expect_execution do
-            syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
             syskit_write task.echo_port, @echo_rotation
         end.to do
             have_one_new_sample(task.frame_port)
@@ -87,8 +112,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
         arrow[0..7] = [0, 255, 0, 255, 0, 255, 0, 255]
         @echo_rotation[:sweep_data] = arrow
         @sensor2ref_pose.orientation = Eigen::Quaternion.new(1, 0, 0, 0)
+
+        syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
         output5 = expect_execution do
-            syskit_write task.sensor2ref_pose_port, @sensor2ref_pose
             syskit_write task.echo_port, @echo_rotation
         end.to do
             have_one_new_sample(task.frame_port)
@@ -109,7 +135,7 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
         samples = 4
         sweep_length = 8
         task.properties.export_config = {
-            time_between_frames: Time.at(0.001),
+            time_between_frames: Time.at(0.0001),
             output_image_size: 512,
             beam_width: 1 / samples * 2 * Math::PI
         }
@@ -123,14 +149,31 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
         samples.times { times_array << time }
         data = []
         input = []
+        input_inverted = []
         data = pattern1 + pattern2
         (samples / 2).times { input.concat(data) }
+        (samples / 2).times { input_inverted.concat(pattern2 + pattern1) }
         @echo = Types.radar_base.Radar.new
         @echo = {
             timestamp: Time.now,
             range: 2.0,
             step_angle: {
                 rad: 2 * Math::PI / samples
+            },
+            start_heading: {
+                rad: 0
+            },
+            sweep_length: sweep_length,
+            sweep_timestamps: times_array,
+            sweep_data: input
+        }
+
+        @echo_inverted = Types.radar_base.Radar.new
+        @echo_inverted = {
+            timestamp: Time.now,
+            range: 2.0,
+            step_angle: {
+                rad: -2 * Math::PI / samples
             },
             start_heading: {
                 rad: 0
@@ -194,9 +237,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
     end
     # rubocop: enable Metrics/AbcSize
 
-    def create_configure_and_start_task
+    def create_configure_task
         task = create_task
-        syskit_configure_and_start(task)
+        syskit_configure(task)
         task
     end
 end
