bf360db8c5e40b4bbc8341dddec4bf6746752523
Wellington Castro <wellington.castro@tidewise.io>
2025-08-24 20:02:04 -0300

    refactor: use stream aligner with echo and sensor pose

--- test/echoes_to_frame_converter_task_test.rb
+++ test/echoes_to_frame_converter_task_test.rb
@@ -1,21 +1,29 @@
 # frozen_string_literal: true
 
+require "syskit/aggregator/test_helpers"
+
 using_task_library "radar_base"
 import_types_from "radar_base"
 import_types_from "base"
 
+DEG2RAD = Math::PI / 180
+
 describe OroGen.radar_base.EchoesToFrameConverterTask do
     run_live
 
+    include Syskit::Aggregator::TestHelpers
+
+    attr_reader :task
+    before do
+        @task = create_configure_task
+    end
+
     it "starts and outputs single radar data" do
-        task = create_configure_task
         syskit_start(task)
 
-        output = expect_execution do
-            syskit_write task.echo_port, @echo
-        end.to do
-            have_one_new_sample(task.frame_port)
-        end
+        write_pose(task, @sensor2ref_pose)
+        output = write_echo(task, @echo)
+
         expected = File.binread(File.join(__dir__, "image1.bin"))
 
         assert_equal expected, output.image.to_a.to_s,
@@ -23,16 +31,13 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
     end
 
     it "starts and outputs multiple radar data for a single frame" do
-        task = create_configure_task
-        task.poll do
-            syskit_write task.echo_port, @echo_part1
-            syskit_write task.echo_port, @echo_part2
-        end
         syskit_start(task)
 
-        output = expect_execution.to do
-            have_one_new_sample(task.frame_port)
-        end
+        write_pose(task, @sensor2ref_pose)
+        now = Time.now
+        write_echo(task, @echo_part1, now)
+        output = write_echo(task, @echo_part2, now + 1)
+
         expected = File.binread(File.join(__dir__, "image2.bin"))
 
         assert_equal expected, output.image.to_a.to_s,
@@ -40,15 +45,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
     end
 
     it "starts and outputs radar data with a negative stepsize" do
-        task = create_configure_task
-        task.poll do
-            syskit_write task.echo_port, @echo_inverted
-        end
         syskit_start(task)
-
-        output = expect_execution.to do
-            have_one_new_sample(task.frame_port)
-        end
+        write_pose(task, @sensor2ref_pose)
+        output = write_echo(task, @echo_inverted)
         expected = File.binread(File.join(__dir__, "image1.bin"))
 
         assert_equal expected, output.image.to_a.to_s,
@@ -56,46 +55,50 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
     end
 
     it "it rotates sample 90 degrees 5 times" do
-        task = create_configure_task
         syskit_start(task)
 
         arrow = []
         64.times { arrow.concat [0] }
         arrow[0..7] = [0, 255, 0, 255, 0, 255, 0, 255]
-        @echo_rotation[:sweep_data] = arrow
-        @sensor2ref_pose.orientation = Eigen::Quaternion.new(1, 0, 0, 0)
+        @echo_rotation.sweep_data = arrow
+        @sensor2ref_pose.orientation =
+            Eigen::Quaternion.from_angle_axis(0, Eigen::Vector3.UnitZ)
 
         write_pose(task, @sensor2ref_pose)
         output1 = write_echo(task, @echo_rotation)
 
         arrow[0..7] = [0, 0, 0, 0, 0, 0, 0, 0]
         arrow[48..55] = [0, 255, 0, 255, 0, 255, 0, 255]
-        @echo_rotation[:sweep_data] = arrow
-        @sensor2ref_pose.orientation = Eigen::Quaternion.new(0.7071068, 0, 0, 0.7071068)
+        @echo_rotation.sweep_data = arrow
+        @sensor2ref_pose.orientation =
+            Eigen::Quaternion.from_angle_axis(90 * DEG2RAD, Eigen::Vector3.UnitZ)
 
         write_pose(task, @sensor2ref_pose)
         output2 = write_echo(task, @echo_rotation)
 
         arrow[48..55] = [0, 0, 0, 0, 0, 0, 0, 0]
         arrow[32..39] = [0, 255, 0, 255, 0, 255, 0, 255]
-        @echo_rotation[:sweep_data] = arrow
-        @sensor2ref_pose.orientation = Eigen::Quaternion.new(0, 0, 0, 1)
+        @echo_rotation.sweep_data = arrow
+        @sensor2ref_pose.orientation =
+            Eigen::Quaternion.from_angle_axis(180 * DEG2RAD, Eigen::Vector3.UnitZ)
 
         write_pose(task, @sensor2ref_pose)
         output3 = write_echo(task, @echo_rotation)
 
         arrow[32..39] = [0, 0, 0, 0, 0, 0, 0, 0]
         arrow[16..23] = [0, 255, 0, 255, 0, 255, 0, 255]
-        @echo_rotation[:sweep_data] = arrow
-        @sensor2ref_pose.orientation = Eigen::Quaternion.new(-0.7071068, 0, 0, 0.7071068)
+        @echo_rotation.sweep_data = arrow
+        @sensor2ref_pose.orientation =
+            Eigen::Quaternion.from_angle_axis(270 * DEG2RAD, Eigen::Vector3.UnitZ)
 
         write_pose(task, @sensor2ref_pose)
         output4 = write_echo(task, @echo_rotation)
 
         arrow[16..23] = [0, 0, 0, 0, 0, 0, 0, 0]
         arrow[0..7] = [0, 255, 0, 255, 0, 255, 0, 255]
-        @echo_rotation[:sweep_data] = arrow
-        @sensor2ref_pose.orientation = Eigen::Quaternion.new(1, 0, 0, 0)
+        @echo_rotation.sweep_data = arrow
+        @sensor2ref_pose.orientation =
+            Eigen::Quaternion.from_angle_axis(0, Eigen::Vector3.UnitZ)
 
         write_pose(task, @sensor2ref_pose)
         output5 = write_echo(task, @echo_rotation)
@@ -115,10 +118,13 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
         samples = 4
         sweep_length = 8
         task.properties.export_config = {
-            time_between_frames: Time.at(0.0001),
+            time_between_frames: Time.at(0),
             output_image_size: 512,
             beam_width: 1 / samples * 2 * Math::PI
         }
+        task.properties.stream_aligner_status_period = 0
+        task.properties.echo_period = 0.02
+        task.properties.sensor2ref_pose_period = 0.01
         pattern = [0, 255]
         pattern1 = []
         pattern2 = []
@@ -133,8 +139,7 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
         data = pattern1 + pattern2
         (samples / 2).times { input.concat(data) }
         (samples / 2).times { input_inverted.concat(pattern2 + pattern1) }
-        @echo = Types.radar_base.Radar.new
-        @echo = {
+        @echo = Types.radar_base.Radar.new(
             timestamp: Time.now,
             range: 2.0,
             step_angle: {
@@ -146,10 +151,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
             sweep_length: sweep_length,
             sweep_timestamps: times_array,
             sweep_data: input
-        }
+        )
 
-        @echo_inverted = Types.radar_base.Radar.new
-        @echo_inverted = {
+        @echo_inverted = Types.radar_base.Radar.new(
             timestamp: Time.now,
             range: 2.0,
             step_angle: {
@@ -161,10 +165,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
             sweep_length: sweep_length,
             sweep_timestamps: times_array,
             sweep_data: input
-        }
+        )
 
-        @echo_part1 = Types.radar_base.Radar.new
-        @echo_part1 = {
+        @echo_part1 = Types.radar_base.Radar.new(
             timestamp: Time.now,
             range: 2.0,
             step_angle: {
@@ -176,10 +179,9 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
             sweep_length: sweep_length,
             sweep_timestamps: times_array,
             sweep_data: input
-        }
+        )
 
-        @echo_part2 = Types.radar_base.Radar.new
-        @echo_part2 = {
+        @echo_part2 = Types.radar_base.Radar.new(
             timestamp: Time.now,
             range: 2.0,
             step_angle: {
@@ -191,9 +193,8 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
             sweep_length: sweep_length,
             sweep_timestamps: times_array,
             sweep_data: input
-        }
-        @echo_rotation = Types.radar_base.Radar.new
-        @echo_rotation = {
+        )
+        @echo_rotation = Types.radar_base.Radar.new(
             timestamp: Time.now,
             range: 2.0,
             step_angle: {
@@ -205,14 +206,15 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
             sweep_length: sweep_length,
             sweep_timestamps: times_array * 2,
             sweep_data: []
-        }
+        )
 
         @sensor2ref_pose = Types.base.samples.RigidBodyState.Invalid
         @sensor2ref_pose.sourceFrame = "world"
         @sensor2ref_pose.targetFrame = "radar"
         @sensor2ref_pose.time = time
         @sensor2ref_pose.position = Eigen::Vector3.new(0, 0, 0)
-        @sensor2ref_pose.orientation = Eigen::Quaternion.new(0.7071068, 0, 0, 0.7071068)
+        @sensor2ref_pose.orientation =
+            Eigen::Quaternion.from_angle_axis(0, Eigen::Vector3.UnitZ)
         task
     end
 
@@ -223,18 +225,24 @@ describe OroGen.radar_base.EchoesToFrameConverterTask do
     end
 
     def write_pose(task, pose)
-        expect_execution do
-            syskit_write task.sensor2ref_pose_port, pose
-        end.to do
-            have_one_new_sample(task.frame_port)
-        end
+        pose.time = Time.now
+        stream_aligner_write(
+            task,
+            task.sensor2ref_pose_port,
+            pose,
+            sample_time_field: "time"
+        )
     end
 
-    def write_echo(task, echo)
-        expect_execution do
-            syskit_write task.echo_port, echo
-        end.to do
-            have_one_new_sample(task.frame_port)
+    def write_echo(task, echo, time = Time.now)
+        echo.timestamp = time
+        stream_aligner_write(
+            task,
+            task.echo_port,
+            echo,
+            sample_time_field: "timestamp"
+        ) do
+            have_one_new_sample task.frame_port
         end
     end
 end
